f1e6db1f1e42616106ae94fcca233ca21cc67b2b
dGakamsky <46941271+dGakamsky@users.noreply.github.com>
2022-11-11 12:09:57 +0000

    API connection works

--- sc2/src/index.js
+++ sc2/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/homeStyle.css';
 import headerImg from './images/SC2.jpg';
@@ -19,12 +19,11 @@ class HomePage extends React.Component{
   getProtossGuide(){
     fetch("https://starcraft-ii.p.rapidapi.com/learning/protoss/page/1/", options)
       .then((res) => res.json())
-      .then(response => console.log(response))
-      .then((response)=> {
+      .then((json)=> {
         this.setState({
-          data: response,
+          data: json,
           dataIsLoaded: true
-          }, []);
+          });
           // return(`<a href= "${response.value[0].webUrl}"> Heres a helpful guide </a>`)
         })
       }
@@ -74,23 +73,23 @@ class HomePage extends React.Component{
         <div class="center, child">
           <h1> This is a title here</h1>
             This is the fifth div
-            <table class="center">
-              <tr>
-                <th>
+            <table class="center, mainDivider">
+              <tr class="center">
+                <td>
                   <div>
                   Terran
                   </div>
-                </th>
-                <th>
+                </td>
+                <td>
                   <div>
                   Protoss
                   </div>
-                </th>
-                <th>
+                </td>
+                <td>
                   <div>
                   Zerg
                   </div>
-                </th>
+                </td>
               </tr>
             </table>
             </div>
@@ -98,8 +97,31 @@ class HomePage extends React.Component{
 
     }
 
-    render() {
+    apiStuff(){
+      this.getProtossGuide()
       const{dataIsLoaded, data} = this.state;
+      if (!dataIsLoaded) return(
+        <div class="center, child">
+            There is no data loaded
+        </div>
+      );
+
+      return(
+        <div>
+        <h1> API data fetched in react </h1>
+          {
+          data.map((item) => (
+            <ol key = {item.id} >
+              Url: {item.webUrl}
+            </ol>
+          )
+          )
+          }
+        </div>
+      );
+    }
+
+    render() {
       return (
         <div class="center, main">
           {this.paragraphOne()}  
@@ -107,7 +129,8 @@ class HomePage extends React.Component{
           {this.paragraphThree()}
           {this.paragraphFour()}
           {this.paragraphFive()}
-          {/* {this.getProtossGuide()} */}
+          {/* {this.apiStuff()} */}
+          <Api/>
         </div>);
       }
 
@@ -120,26 +143,31 @@ class HomePage extends React.Component{
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HomePage />);
-const options = {
-  return: {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '8ad2018c6amshafa81a681feb275p197c9ejsnd889565de76c',
-    'X-RapidAPI-Host': 'starcraft-ii.p.rapidapi.com'
-  }
-}
-}
+root.render(
+<HomePage />);
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '80e1fd2c48msh25d4d97a364c2d1p1244e4jsneffbf1adca25',
+		'X-RapidAPI-Host': 'starcraft-ii.p.rapidapi.com'
+	}
+};
 
 //==============================================
-/*
-  <React.StrictMode>
-    <div class="center, main">
-      <div class="center, child">
-      This is some text 
-      {this.data[0]}
-      </div>
-    </div>
-  </React.StrictMode>
-*/ 
+
+
+function Api(){
+  const[ReturnData, StoreData] = useState("");
+  useEffect(()=>{
+    fetch("https://starcraft-ii.p.rapidapi.com/learning/protoss/page/1/",options)
+      .then((res) => res.json())
+      .then((response)=> {
+        StoreData(response.value[0].originalUrl)
+        })
+      .catch((err)=> console.error(err))
+      })
+    return(<div>
+      <a href= {ReturnData}> Heres a helpful guide </a>
+    </div>);
+}
